f59b97633839d8982f5270ab755698537742e101
David Parseen <hello@parseen.dev>
2024-08-23 08:37:00 +0300

    refactor: Add form validation and error messages

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -9,6 +9,12 @@
   let isUsernameAvailable = false;
 
   let debounceTimer: NodeJS.Timeout;
+  const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+  $: isValid =
+    username?.length > 2 && username?.length < 17 && re.test(username);
+  $: isTouched = username?.length > 0;
+  $: isTaken = isValid && !isUsernameAvailable && !loading;
 
   async function checkUsernameAvailability() {
     clearTimeout(debounceTimer);
@@ -22,7 +28,6 @@
   }
 
   async function confirmUsername() {
-    console.log("confirming username", username);
     const batch = writeBatch(db);
     batch.set(doc(db, "usernames", username), { uid: $user?.uid });
     batch.set(doc(db, "users", $user!.uid), {
@@ -51,22 +56,29 @@
       on:submit|preventDefault={confirmUsername}
     >
       <h2 class="card-title">Username</h2>
-      <label class="label cursor-pointer flex items-center gap-2">
-        <User />
+      <label
+        class:input-success={isUsernameAvailable && isValid && !loading}
+        class:input-error={!isValid && isTouched}
+        class:input-warning={isTaken}
+        class="input input-bordered flex items-center gap-2"
+      >
         <input
           type="text"
-          class="grow input input-bordered"
-          class:input-success={isUsernameAvailable && username && !loading}
-          class:input-error={!isUsernameAvailable && username && !loading}
+          class="grow"
           placeholder="Username"
           bind:value={username}
           on:input={checkUsernameAvailability}
         />
+        <User class="opacity-70" />
       </label>
       {#if username}
-        {#if loading}
+        {#if !isValid && isTouched}
+          <p class="text-error text-sm">
+            must be 3-17 characters long, alphanumeric only
+          </p>
+        {:else if loading && isTouched}
           <p class="text-base-content">Checking availability...</p>
-        {:else if isUsernameAvailable}
+        {:else if isUsernameAvailable && isValid}
           <p class="text-success">{username} is available!</p>
         {:else}
           <p class="text-error">{username} is unavailable</p>
@@ -75,7 +87,7 @@
       <button
         type="submit"
         class="btn btn-primary"
-        disabled={!isUsernameAvailable || !username || loading}
+        disabled={!isUsernameAvailable || !isValid || loading}
         >Confirm Username {username && "@"}{username}</button
       >
     </form>
