d1ddf9dcd9b9d0454637e8024587b7604bac436a
David Parseen <hello@parseen.dev>
2024-08-23 22:48:48 +0300

    feat: Add local variable for user id and hook for basic authorization on server actions

--- src/routes/[username]/bio/+page.server.ts
+++ src/routes/[username]/bio/+page.server.ts
@@ -1,23 +1,48 @@
-import type { PageServerLoad } from "./$types";
-import { adminAuth, adminDB } from "$lib/firebase/admin";
-import { error } from "@sveltejs/kit";
+import type { PageServerLoad, Actions } from "./$types";
+import { adminDB } from "$lib/firebase/admin";
+import { error, fail, redirect } from "@sveltejs/kit";
 
-export const load = (async ({ cookies }) => {
+export const load = (async ({ locals, params }) => {
 
-    const sessionCookie = cookies.get('__session');
+    const uid = locals.userID;
 
-    try {
-        const decodedClaims = await adminAuth.verifySessionCookie(sessionCookie!);
-        const userDoc = await adminDB.collection('users').doc(decodedClaims.uid).get();
-        const userData = userDoc.data();
+    if (!uid) {
+        throw redirect(301, "/login");
+    }
 
-        return {
-            bio: userData?.bio,
+    const userDoc = await adminDB.collection("users").doc(uid).get();
+    const { username, bio } = userDoc.data()!;
+
+    if (params.username !== username) {
+        throw error(401, "That username does not belong to you");
+    }
+
+    return {
+        bio,
+    };
+}) satisfies PageServerLoad;
+
+export const actions = {
+    default: async ({ locals, request, params }) => {
+
+        const uid = locals.userID;
+
+        const data = await request.formData();
+        const bio = data.get('bio');
+
+        const userRef = adminDB.collection("users").doc(uid!);
+        const { username } = (await userRef.get()).data()!;
+
+        if (params.username !== username) {
+            throw error(401, "That username does not belong to you");
         }
 
-    } catch (e) {
-        console.log(e)
-        // redirect(301, '/login');
-        throw error(401, 'Unauthorized request!')
-    }
-}) satisfies PageServerLoad;
+        if (bio!.toString().length > 260) {
+            return fail(400, { problem: "Bio must be less than 260 characters" });
+        }
+
+        await userRef.update({
+            bio,
+        });
+    },
+} satisfies Actions;
